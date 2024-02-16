58f3d0a4098bf9294cbea4f0d4bb8e5d8061558d
MohamedEmadFahmy <mohamedemadfahmy0@gmail.com>
2024-02-16 20:20:38 +0200

    finish the history of javascript with code

--- Javascript/ES6 Modules/ES6 Modules.js
+++ Javascript/ES6 Modules/ES6 Modules.js
@@ -1,1 +1,5 @@
-console.log("hello");
+// const moment = require("moment");
+import moment from "moment";
+
+
+console.log(moment().startOf("day").fromNow());
---
+++ Javascript/ES6 Modules/webpack.config.js
@@ -0,0 +1,9 @@
+// webpack.config.js  
+module.exports = {  
+    mode: 'development',  
+    entry: './ES6 Modules.js',  
+    output: {  
+      filename: 'main.js',  
+      publicPath: 'dist'  
+    }  
+  };
