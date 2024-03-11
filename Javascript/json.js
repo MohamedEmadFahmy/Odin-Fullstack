bad1adfab9bd3c6ee4160e6f79417b34cafa7451
MohamedEmadFahmy <mohamedemadfahmy0@gmail.com>
2024-03-11 22:38:41 +0200

    Fix spacing issue

--- Javascript/json.js
+++ Javascript/json.js
@@ -1,24 +1,22 @@
 async function populate() {
-    const requestURL =
-      "https://mdn.github.io/learning-area/javascript/oojs/json/superheroes.json";
-    const request = new Request(requestURL);
-  
-    const response = await fetch(request);
-    const superHeroesText = await response.text();
+	const requestURL =
+		"https://mdn.github.io/learning-area/javascript/oojs/json/superheroes.json";
+	const request = new Request(requestURL);
 
-    const superHeroes = JSON.parse(superHeroesText);
-  
-    console.log(superHeroesText);     // JSON string
-    console.log(superHeroes);         // JSON object
+	const response = await fetch(request);
+	const superHeroesText = await response.text();
 
-    let myObj = { name: "Chris", age: 38 };
+	const superHeroes = JSON.parse(superHeroesText);
 
-    let myJSON = JSON.stringify(myObj);
+	console.log(superHeroesText); // JSON string
+	console.log(superHeroes); // JSON object
 
-    console.log(myJSON);  // JSON string
-    console.log(myObj);  // JSON string
+	let myObj = { name: "Chris", age: 38 };
 
+	let myJSON = JSON.stringify(myObj);
+
+	console.log(myJSON); // JSON string
+	console.log(myObj); // JSON string
 }
 
 populate();
-  
