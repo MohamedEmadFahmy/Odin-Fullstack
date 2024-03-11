6ac54e2080f2e7e42fe69c0511c6a7a98446fa6e
MohamedEmadFahmy <mohamedemadfahmy0@gmail.com>
2024-03-11 21:57:11 +0200

    Finish JSON lesson

---
+++ Javascript/json.js
@@ -0,0 +1,24 @@
+async function populate() {
+    const requestURL =
+      "https://mdn.github.io/learning-area/javascript/oojs/json/superheroes.json";
+    const request = new Request(requestURL);
+  
+    const response = await fetch(request);
+    const superHeroesText = await response.text();
+
+    const superHeroes = JSON.parse(superHeroesText);
+  
+    console.log(superHeroesText);     // JSON string
+    console.log(superHeroes);         // JSON object
+
+    let myObj = { name: "Chris", age: 38 };
+
+    let myJSON = JSON.stringify(myObj);
+
+    console.log(myJSON);  // JSON string
+    console.log(myObj);  // JSON string
+
+}
+
+populate();
+  
